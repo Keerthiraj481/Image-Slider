9f7f25a59c77aed84136862991962734767483bb
keerthi <keerthiraj.4801@gmail.com>
2023-09-19 12:24:11 +0530

    Adding the image slider page

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+const imageEle = document.querySelector(".image");
+const nextBtnEle = document.getElementById("btn-next");
+const previousBtnEle = document.getElementById("btn-previous");
+let index = 0;
+const imageArr = [
+  "image-0.jpg",
+  "image-1.jpg",
+  "image-2.jpg",
+  "image-3.jpg",
+  "image-4.jpg",
+];
+const str = "./images/";
+function init() {
+  imageEle.src = `${str}${imageArr[0]}`;
+}
+const changeImage = () => {
+  if (index < 0) {
+    index = imageArr.length - 1;
+  } else if (index >= 0 && index < imageArr.length) {
+  } else if (index == imageArr.length) {
+    index = 0;
+  }
+  imageEle.src = `${str}${imageArr[index]}`;
+};
+nextBtnEle.addEventListener("click", () => {
+  index += 1;
+  changeImage();
+});
+
+previousBtnEle.addEventListener("click", () => {
+  index -= 1;
+  changeImage();
+});
+
+init();
